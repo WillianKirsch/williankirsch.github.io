aea90551f8923f83850401a4e2611386b9f0790c
Willian Kirsch <willian.kirsch@gmail.com>
2022-05-11 09:40:07 -0300

    up

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "03bd9d8b08ff771b56675793c07567eb",
-"index.html": "9e7fb08ebd8481463c95efbec8d6488c",
-"/": "9e7fb08ebd8481463c95efbec8d6488c",
-"main.dart.js": "18cd5fbe9802373bf59644538c68bd65",
+"index.html": "3cb823b1939d4884320e3e18be53e9d3",
+"/": "3cb823b1939d4884320e3e18be53e9d3",
+"main.dart.js": "45d6f01b80692c91229724de4206b88b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
